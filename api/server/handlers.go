4cf3b189a36a0feebce26c981129f14e075b51de
jonas <jonas@satalia.com>
2018-12-09 14:37:57 +0000

    restructuring

---
+++ api/server/handlers.go
@@ -0,0 +1,101 @@
+package server
+
+import (
+	"errors"
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"mime/multipart"
+	"net/http"
+)
+
+type mainPageVariables struct {
+}
+
+type workPageVariables struct {
+	Filename string
+}
+
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("web/templates/main.html")
+	if err != nil {
+		log.Fatalf("template parsing error: %v", err)
+	}
+
+	testPageVariables := mainPageVariables{}
+
+	err = t.Execute(w, testPageVariables)
+	if err != nil {
+		log.Fatalf("populating the template failed: %v", err)
+	}
+}
+
+func imageUploadHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+	r.ParseForm()
+	image, handle, err := r.FormFile("picture")
+	if err != nil {
+		fmt.Fprintf(w, "%v", err)
+		return
+	}
+	defer image.Close()
+	fmt.Print("Uploading image ", handle.Filename, "...")
+
+	err = checkFileType(handle)
+	if err != nil {
+		fmt.Println("error!\nImage could not be uploaded: %", err)
+		return
+	}
+
+	saveImage(w, image, handle)
+	fmt.Println("success!")
+
+	http.Redirect(w, r, "/work?image="+handle.Filename, http.StatusSeeOther)
+}
+
+func checkFileType(handle *multipart.FileHeader) error {
+	mimeType := handle.Header.Get("Content-Type")
+
+	switch mimeType {
+	case "image/jpeg", "image/png":
+		return nil
+	default:
+		return errors.New("mimeType " + mimeType + " not supported")
+	}
+}
+
+func saveImage(w http.ResponseWriter, image multipart.File, handle *multipart.FileHeader) {
+	data, err := ioutil.ReadAll(image)
+	if err != nil {
+		fmt.Fprintf(w, "%v", err)
+		return
+	}
+
+	err = ioutil.WriteFile("./uploads/"+handle.Filename, data, 0666)
+	if err != nil {
+		fmt.Fprintf(w, "%v", err)
+		return
+	}
+}
+
+func workHandler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("web/templates/work.html")
+	if err != nil {
+		log.Fatalf("work template parsing error: %v", err)
+	}
+
+	r.ParseForm()
+	workPageVariables := workPageVariables{
+		Filename: r.Form["image"][0],
+	}
+
+	err = t.Execute(w, workPageVariables)
+	if err != nil {
+		log.Fatalf("populating the template failed: %v", err)
+	}
+}
--- api/server/server.go
+++ api/server/server.go
@@ -2,19 +2,9 @@ package server
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
-	"mime/multipart"
 	"net/http"
-	"text/template"
 )
-
-type mainPageVariables struct {
-}
-
-type workPageVariables struct {
-	Filename string
-}
 
 // Serve serves the server
 func Serve(port string) {
@@ -28,80 +18,4 @@ func Serve(port string) {
 
 	fmt.Println("Running server on http://localhost" + port)
 	log.Fatal(http.ListenAndServe(port, nil))
-}
-
-func defaultHandler(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("web/templates/main.html")
-	if err != nil {
-		log.Fatalf("template parsing error: %v", err)
-	}
-
-	testPageVariables := mainPageVariables{}
-
-	err = t.Execute(w, testPageVariables)
-	if err != nil {
-		log.Fatalf("populating the template failed: %v", err)
-	}
-}
-
-func imageUploadHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Redirect(w, r, "/", http.StatusSeeOther)
-		return
-	}
-
-	r.ParseForm()
-
-	image, handle, err := r.FormFile("pic")
-	if err != nil {
-		fmt.Fprintf(w, "%v", err)
-		return
-	}
-	defer image.Close()
-
-	mimeType := handle.Header.Get("Content-Type")
-	fmt.Print("Uploading image ", handle.Filename, "...")
-
-	switch mimeType {
-	case "image/jpeg":
-		saveImage(w, image, handle)
-	case "image/png":
-		saveImage(w, image, handle)
-	default:
-		fmt.Println("file type ", mimeType, " not supported")
-	}
-
-	http.Redirect(w, r, "/work?image="+handle.Filename, http.StatusSeeOther)
-}
-
-func workHandler(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("web/templates/work.html")
-	if err != nil {
-		log.Fatalf("work template parsing error: %v", err)
-	}
-
-	r.ParseForm()
-	workPageVariables := workPageVariables{
-		Filename: r.Form["image"][0],
-	}
-
-	err = t.Execute(w, workPageVariables)
-	if err != nil {
-		log.Fatalf("populating the template failed: %v", err)
-	}
-}
-
-func saveImage(w http.ResponseWriter, image multipart.File, handle *multipart.FileHeader) {
-	data, err := ioutil.ReadAll(image)
-	if err != nil {
-		fmt.Fprintf(w, "%v", err)
-		return
-	}
-
-	err = ioutil.WriteFile("./uploads/"+handle.Filename, data, 0666)
-	if err != nil {
-		fmt.Fprintf(w, "%v", err)
-		return
-	}
-	fmt.Println("success!")
 }
